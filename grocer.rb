9d8d2954cba36ef5ef429f5ef9852663e9bfeebc
evancarlsen <evan@evancarlsen.com>
2019-06-23 12:40:10 -0700

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,68 @@
 def consolidate_cart(cart)
-  # code here
+
+	cons_cart = {}
+
+	cart.each do |item_hash|
+		item_name = item_hash.keys[0]
+		if cons_cart[item_name] == nil
+			cons_cart[item_name] = item_hash[item_name]
+			cons_cart[item_name][:count] = 1
+		else
+			current_count = cons_cart[item_name][:count]
+			cons_cart[item_name][:count] = current_count + 1
+		end
+	end
+	cons_cart
 end
 
-def apply_coupons(cart, coupons)
-  # code here
+def apply_coupons(cart,coupons)
+	coupons.each do |coup_hash|
+		item_name = coup_hash[:item]
+		new_name = "#{item_name} W/COUPON"
+		coupon_count = coup_hash[:num]
+		price_per_item = coup_hash[:cost]/coupon_count
+    if cart[item_name] != nil
+      if cart[item_name][:count] >= coupon_count
+        cart[item_name][:count] -= coupon_count
+        if cart[new_name] == nil
+          cart[new_name] = {
+            :price => price_per_item,
+            :clearance => cart[item_name][:clearance],
+            :count => coupon_count
+          }
+        else
+          cart[new_name][:count] += coupon_count
+        end
+      end
+		end
+
+	end
+	cart
 end
 
 def apply_clearance(cart)
-  # code here
+	cart.each do |item, item_hash|
+		if item_hash[:clearance] == true
+			old_price = item_hash[:price]
+
+			new_price = (old_price * 0.80).round(2)
+			item_hash[:price] = new_price
+		end
+	end
+
+	cart
 end
 
-def checkout(cart, coupons)
-  # code here
+def checkout(cart,coupons)
+	cons_cart = consolidate_cart(cart)
+	coup_cart = apply_coupons(cons_cart,coupons)
+	clear_cart = apply_clearance(coup_cart)
+	total_cost = 0
+	clear_cart.each do |item, item_hash|
+		total_cost += item_hash[:count] * item_hash[:price]
+  end
+  if total_cost > 100
+    total_cost *= 0.9
+  end
+	total_cost.round(2)
 end
--- random_grocery_generator.rb
+++ random_grocery_generator.rb
@@ -1,5 +1,5 @@
 require_relative 'grocer'
-
+require 'pry'
 def items
 	[
 		{"AVOCADO" => {:price => 3.00, :clearance => true}},
@@ -38,8 +38,77 @@ def generate_coupons
 	end
 end
 
+def consolidate_cart(cart)
+	binding.pry
+	cons_cart = {}
+	binding.pry
+	cart.each do |item_hash|
+		item_name = item_hash.keys[0]
+		if cons_cart[item_name] == nil
+			cons_cart[item_name] = item_hash[item_name]
+			cons_cart[item_name][:count] = 1
+		else
+			current_count = cons_cart[item_name][:count]
+			cons_cart[item_name][:count] = current_count + 1
+		end
+	end
+	cons_cart
+end
+
+#[{:item=>"AVOCADO", :num=>2, :cost=>5.0}]
+def apply_coupons(cart,coupons)
+	coupons.each do |coup_hash|
+		item_name = coup_hash[:item]
+		new_name = "#{item_name} W/COUPON"
+		coupon_count = coup_hash[:num]
+		price_per_item = coup_hash[:cost]/coupon_count
+		if cart[item_name] != nil
+			cart[item_name][:count] -= coupon_count
+			cart[new_name] = {
+				:price => price_per_item,
+				:clearance => cart[item_name][:clearance],
+				:count => coupon_count
+			}
+		end
+
+	end
+	cart
+end
+
+def apply_clearance(cart)
+	cart.each do |item, item_hash|
+		if item_hash[:clearance] == true
+			old_price = item_hash[:price]
+
+			new_price = (old_price * 0.80).round(2)
+			item_hash[:price] = new_price
+		end
+	end
+
+	cart
+end
+
+def checkout(cart,coupons)
+	cons_cart = consolidate_cart(cart)
+	coup_cart = apply_coupons(cons_cart,coupons)
+	clear_cart = apply_clearance(coup_cart)
+	total_cost = 0
+	clear_cart.each do |item, item_hash|
+		total_cost += item_hash[:count] * item_hash[:price]
+	end
+	total_cost.round(2)
+end
+ 
 cart = generate_cart
+# puts "----------------------"
+# puts cart
+# puts "----------------------"
 coupons = generate_coupons
+
+new_cart = consolidate_cart(cart)
+
+
+puts new_cart
 
 puts "Items in cart"
 cart.each do |item|
@@ -49,9 +118,11 @@ cart.each do |item|
 	puts "=" * 10
 end
 
+
 puts "Coupons on hand"
 coupons.each do |coupon|
 	puts "Get #{coupon[:item].capitalize} for #{coupon[:cost]} when you by #{coupon[:num]}"
 end
 
-puts "Your total is #{checkout(cart: cart, coupons: coupons)}"
+puts "Your total is #{checkout(cart, coupons)}"
+
